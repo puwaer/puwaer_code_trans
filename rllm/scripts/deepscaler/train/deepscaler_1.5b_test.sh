6e9c343706f2b5d90eb845997c6a11939b29f47e
puwaer <y_50uki@outlook.jp>
2025-04-19 15:33:14 +0900

    1.5b_test

--- rllm/scripts/deepscaler/train/deepscaler_1.5b_test.sh
+++ rllm/scripts/deepscaler/train/deepscaler_1.5b_test.sh
@@ -28,16 +28,16 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/rllm/data/deepscaler_train.parquet \
     data.val_files=$HOME/rllm/data/aime.parquet \
-    data.train_batch_size=2 \
-    data.val_batch_size=2 \
+    data.train_batch_size=1 \
+    data.val_batch_size=1 \
     data.max_prompt_length=1024  \
     data.max_response_length=8192 \
     actor_rollout_ref.model.path=$MODEL_PATH \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=1 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768  \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=16384  \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -49,10 +49,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.val_temperature=0.6 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \
-    actor_rollout_ref.rollout.n=4 \
-    actor_rollout_ref.rollout.n_val=4 \
-    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.n=1 \
+    actor_rollout_ref.rollout.n_val=1 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=False \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
@@ -64,4 +64,6 @@ python3 -m verl.trainer.main_ppo \
     trainer.save_freq=20 \
     trainer.test_freq=20 \
     trainer.default_hdfs_dir=null \
-    trainer.total_epochs=30 "${@:1}"
+    trainer.total_epochs=1 "${@:1}"
+
+
