7b3396ab542a804e8a33889d3e9f77679dea1caa
puwaer <y_50uki@outlook.jp>
2025-04-23 14:59:58 +0900

    paramater

--- TinyZero/scripts/train_tiny_zero_qwen_1.5b.sh
+++ TinyZero/scripts/train_tiny_zero_qwen_1.5b.sh
@@ -1,17 +1,17 @@
 python3 -m verl.trainer.main_ppo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
-    data.train_batch_size=256 \
-    data.val_batch_size=1312 \
+    data.train_batch_size=64 \
+    data.val_batch_size=256 \
     data.max_prompt_length=256 \
     data.max_response_length=1024 \
     actor_rollout_ref.model.path=$BASE_MODEL \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=4 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
     actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
@@ -28,4 +28,4 @@ python3 -m verl.trainer.main_ppo \
     trainer.test_freq=100 \
     trainer.project_name=TinyZero \
     trainer.experiment_name=$EXPERIMENT_NAME \
-    trainer.total_epochs=15 2>&1 | tee verl_demo.log
+    trainer.total_epochs=10 2>&1 | tee verl_demo.log
